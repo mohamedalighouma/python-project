2b43eb6f690fe171ff3dfc2bead6a10ca26d248b
Mohamed Ali GHOUMA <uid1354@focus.com.tn>
2021-09-08 17:09:06 +0100

    unit  tests commit

--- TestStringmethods.py
+++ TestStringmethods.py
@@ -3,7 +3,23 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        self.assertEqual(True, True)  # add assertion here
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
 
 
 if __name__ == '__main__':
